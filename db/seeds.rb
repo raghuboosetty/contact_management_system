39e8c070741d932b519098baf5a87034a081e9df
Raghu Boosetty <raghu.boos@gmail.com>
2018-12-21 13:37:43 +0530

    update seed with add on employee info

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 unless Employee.find_by_email('employee_1@inmar.com')
   puts 'Seeding Employee...'
-  Employee.create(email: 'employee_1@inmar.com', password: 'password', password_confirmation: 'password')
+  Employee.create(first_name: 'employee', last_name: 'one', aadhar: 123445544, email: 'employee_1@inmar.com', password: 'password', password_confirmation: 'password')
 end
