135dee05db7cb80860ef09cbb02fd4e7f70fdc13
Raghu Boosetty <raghu.boos@gmail.com>
2018-12-21 12:25:36 +0530

    authenticate employee and seed

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :contacts
   resources :groups
   devise_for :employees
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'groups#index'
 end
